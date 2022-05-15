39476ae8819b865829fc792d526fb352488fbc3b
Guilherme Souza <grsouza@pm.me>
2022-05-15 07:31:48 -0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/supabase-community/gotrue-swift", .exactItem("0.0.3")),
+    .package(url: "https://github.com/supabase-community/gotrue-swift", .exactItem("0.0.4")),
     .package(
       name: "SupabaseStorage", url: "https://github.com/supabase/storage-swift.git", .exact("0.0.2")
     ),
