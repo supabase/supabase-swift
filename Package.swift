0e7b96553f5ce36ddf7c8ba4f154daf9933f2483
Guilherme Souza <grsouza@pm.me>
2022-07-08 07:39:48 -0300

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -19,11 +19,10 @@ let package = Package(
     )
   ],
   dependencies: [
-    .package(url: "https://github.com/supabase-community/gotrue-swift", branch: "main"),
+    .package(url: "https://github.com/supabase-community/gotrue-swift", from: "0.0.7"),
     .package(url: "https://github.com/supabase-community/storage-swift.git", from: "0.0.2"),
     .package(url: "https://github.com/supabase-community/realtime-swift.git", from: "0.0.1"),
-    .package(
-      url: "https://github.com/supabase-community/postgrest-swift", branch: "request-adapters"),
+    .package(url: "https://github.com/supabase-community/postgrest-swift", from: "0.0.5"),
   ],
   targets: [
     .target(
