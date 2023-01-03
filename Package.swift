bc483c99516b84842d7882772a37f5b8759f7b8a
daymein <daymeing@gmail.com>
2023-01-03 07:26:34 -0600

    updated SupabaseClient and Package to fix build errors

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,14 +19,11 @@ let package = Package(
     ),
   ],
   dependencies: [
-    .package(url: "https://github.com/supabase-community/gotrue-swift", from: "0.0.7"),
+    .package(url: "https://github.com/supabase-community/gotrue-swift", from: "0.1.0"),
     .package(url: "https://github.com/supabase-community/storage-swift.git", branch: "main"),
     .package(url: "https://github.com/supabase-community/realtime-swift.git", from: "0.0.1"),
-    .package(
-      url: "https://github.com/supabase-community/postgrest-swift",
-      branch: "master"
-    ),
-    .package(url: "https://github.com/supabase-community/functions-swift", branch: "main"),
+    .package(url: "https://github.com/supabase-community/postgrest-swift", from: "1.0.0"),
+    .package(url: "https://github.com/supabase-community/functions-swift", from: "0.2.0"),
   ],
   targets: [
     .target(
@@ -37,6 +34,14 @@ let package = Package(
         .product(name: "Realtime", package: "realtime-swift"),
         .product(name: "PostgREST", package: "postgrest-swift"),
         .product(name: "Functions", package: "functions-swift"),
+      ],
+      swiftSettings: [
+        .unsafeFlags(
+          [
+            "-warn-concurrency",
+            "-enable-actor-data-race-checks",
+          ]
+        )
       ]
     ),
   ]
