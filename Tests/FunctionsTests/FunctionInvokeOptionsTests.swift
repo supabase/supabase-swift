445a83197898651b9d526daabedb8200806c2ab6
Guilherme Souza <grsouza@pm.me>
2023-11-15 05:25:16 -0300

    fix: functions overrides headers

--- Tests/FunctionsTests/FunctionInvokeOptionsTests.swift
+++ Tests/FunctionsTests/FunctionInvokeOptionsTests.swift
@@ -3,24 +3,35 @@ import XCTest
 @testable import Functions
 
 final class FunctionInvokeOptionsTests: XCTestCase {
-  func testStringBody() {
+  func test_initWithStringBody() {
     let options = FunctionInvokeOptions(body: "string value")
     XCTAssertEqual(options.headers["Content-Type"], "text/plain")
     XCTAssertNotNil(options.body)
   }
 
-  func testDataBody() {
+  func test_initWithDataBody() {
     let options = FunctionInvokeOptions(body: "binary value".data(using: .utf8)!)
     XCTAssertEqual(options.headers["Content-Type"], "application/octet-stream")
     XCTAssertNotNil(options.body)
   }
 
-  func testEncodableBody() {
+  func test_initWithEncodableBody() {
     struct Body: Encodable {
       let value: String
     }
     let options = FunctionInvokeOptions(body: Body(value: "value"))
     XCTAssertEqual(options.headers["Content-Type"], "application/json")
+    XCTAssertNotNil(options.body)
+  }
+
+  func test_initWithCustomContentType() {
+    let boundary = "Boundary-\(UUID().uuidString)"
+    let contentType = "multipart/form-data; boundary=\(boundary)"
+    let options = FunctionInvokeOptions(
+      headers: ["Content-Type": contentType],
+      body: "binary value".data(using: .utf8)!
+    )
+    XCTAssertEqual(options.headers["Content-Type"], contentType)
     XCTAssertNotNil(options.body)
   }
 }
