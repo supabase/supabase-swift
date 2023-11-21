3e6ff51e9f63d84acb21e4b9c9ae79c605e8a5f5
Guilherme Souza <grsouza@pm.me>
2023-11-20 21:03:59 -0300

    Fix supabaseURL param

--- Examples/UserManagement/Supabase.swift
+++ Examples/UserManagement/Supabase.swift
@@ -9,6 +9,6 @@ import Foundation
 import Supabase
 
 let supabase = SupabaseClient(
-  supabaseURL: URL(string: "https://PROJECT_ID.supabase.co")!,
+  supabaseURL: "https://PROJECT_ID.supabase.co",
   supabaseKey: "YOUR_SUPABASE_ANON_KEY"
 )
