b8611cfc523895d2849f75574b331339e48e29cf
Guilherme Souza <grsouza@pm.me>
2023-12-17 09:42:11 -0300

    Prepare examples project for running on CI

--- Examples/Examples/_Secrets.swift
+++ Examples/Examples/_Secrets.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 enum Secrets {
-  static let supabaseURL = URL(string: "SUPABASE_URL")!
+  static let supabaseURL = URL(string: "http://localhost:54321")!
   static let supabaseAnonKey = "SUPABASE_ANON_KEY"
 }
