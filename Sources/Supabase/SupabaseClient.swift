ec1f202708ef52854d64a0a441c2b92f1258a50b
Guilherme Souza <grsouza@pm.me>
2024-04-15 12:35:07 -0300

    chore: remove deprecated database var from supabase client

--- Sources/Supabase/SupabaseClient.swift
+++ Sources/Supabase/SupabaseClient.swift
@@ -30,16 +30,6 @@ public final class SupabaseClient: @unchecked Sendable {
   /// by access policies.
   public let auth: AuthClient
 
-  /// Database client for Supabase.
-  @available(
-    *,
-    deprecated,
-    message: "Direct access to database is deprecated, please use one of the available methods such as, SupabaseClient.from(_:), SupabaseClient.rpc(_:params:), or SupabaseClient.schema(_:)."
-  )
-  public var database: PostgrestClient {
-    rest
-  }
-
   private lazy var rest = PostgrestClient(
     url: databaseURL,
     schema: options.db.schema,
