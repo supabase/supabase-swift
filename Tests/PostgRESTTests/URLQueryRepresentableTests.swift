2d4a09e627e36e93eeda52e1823eaedd95601c1c
Guilherme Souza <grsouza@pm.me>
2024-04-16 13:41:38 -0300

    test: add tests for new methods

--- Tests/PostgRESTTests/URLQueryRepresentableTests.swift
+++ Tests/PostgRESTTests/URLQueryRepresentableTests.swift
@@ -8,9 +8,19 @@ final class URLQueryRepresentableTests: XCTestCase {
     XCTAssertEqual(queryValue, "{is:online,faction:red}")
   }
 
-  func testDictionary() {
-    let dictionary = ["postalcode": 90210]
-    let queryValue = dictionary.queryValue
-    XCTAssertEqual(queryValue, "{\"postalcode\":90210}")
+  func testAnyJSON() {
+    XCTAssertEqual(
+      AnyJSON.array(["is:online", "faction:red"]).queryValue,
+      "{is:online,faction:red}"
+    )
+    XCTAssertEqual(
+      AnyJSON.object(["postalcode": 90210]).queryValue,
+      "{\"postalcode\":90210}"
+    )
+    XCTAssertEqual(AnyJSON.string("string").queryValue, "string")
+    XCTAssertEqual(AnyJSON.double(3.14).queryValue, "3.14")
+    XCTAssertEqual(AnyJSON.integer(3).queryValue, "3")
+    XCTAssertEqual(AnyJSON.bool(true).queryValue, "true")
+    XCTAssertEqual(AnyJSON.null.queryValue, "NULL")
   }
 }
