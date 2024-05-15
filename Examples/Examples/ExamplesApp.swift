41dd4ed77cd7e64eabbe5508df47e7d1e23bddc3
Guilherme Souza <grsouza@pm.me>
2024-05-15 15:09:13 -0300

    feat(auth): automatically load session from deep link

--- Examples/Examples/ExamplesApp.swift
+++ Examples/Examples/ExamplesApp.swift
@@ -9,8 +9,35 @@ import GoogleSignIn
 import Supabase
 import SwiftUI
 
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+  ) -> Bool {
+    supabase.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
+    supabase.application(app, open: url, options: options)
+  }
+
+  func application(_: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options _: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    let configuration = UISceneConfiguration(name: nil, sessionRole: connectingSceneSession.role)
+    configuration.delegateClass = SceneDelegate.self
+    return configuration
+  }
+}
+
+class SceneDelegate: UIResponder, UISceneDelegate {
+  func scene(_ scene: UIScene, openURLContexts URLContexts: Set<UIOpenURLContext>) {
+    supabase.scene(scene, openURLContexts: URLContexts)
+  }
+}
+
 @main
 struct ExamplesApp: App {
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
   var body: some Scene {
     WindowGroup {
       RootView()
