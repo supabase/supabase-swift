17f08671035dc0c2430986803bf4f89ddc926e31
Guilherme Souza <grsouza@pm.me>
2024-10-01 10:12:34 -0300

    keep #isolation to swift 6.0 only

--- Sources/Helpers/SupabaseLogger.swift
+++ Sources/Helpers/SupabaseLogger.swift
@@ -171,23 +171,45 @@ extension SupabaseLogger {
   }
 }
 
-@inlinable
-@discardableResult
-package func trace<R: Sendable>(
-  using logger: (any SupabaseLogger)?,
-  _ operation: () async throws -> R,
-  isolation _: isolated (any Actor)? = #isolation,
-  fileID: StaticString = #fileID,
-  function: StaticString = #function,
-  line: UInt = #line
-) async rethrows -> R {
-  logger?.debug("begin", fileID: fileID, function: function, line: line)
-  defer { logger?.debug("end", fileID: fileID, function: function, line: line) }
+#if compiler(>=6.0)
+  @inlinable
+  @discardableResult
+  package func trace<R: Sendable>(
+    using logger: (any SupabaseLogger)?,
+    _ operation: () async throws -> R,
+    isolation _: isolated (any Actor)? = #isolation,
+    fileID: StaticString = #fileID,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) async rethrows -> R {
+    logger?.debug("begin", fileID: fileID, function: function, line: line)
+    defer { logger?.debug("end", fileID: fileID, function: function, line: line) }
 
-  do {
-    return try await operation()
-  } catch {
-    logger?.debug("error: \(error)", fileID: fileID, function: function, line: line)
-    throw error
+    do {
+      return try await operation()
+    } catch {
+      logger?.debug("error: \(error)", fileID: fileID, function: function, line: line)
+      throw error
+    }
   }
-}
+#else
+  @inlinable
+  @discardableResult
+  package func trace<R: Sendable>(
+    using logger: (any SupabaseLogger)?,
+    _ operation: () async throws -> R,
+    fileID: StaticString = #fileID,
+    function: StaticString = #function,
+    line: UInt = #line
+  ) async rethrows -> R {
+    logger?.debug("begin", fileID: fileID, function: function, line: line)
+    defer { logger?.debug("end", fileID: fileID, function: function, line: line) }
+
+    do {
+      return try await operation()
+    } catch {
+      logger?.debug("error: \(error)", fileID: fileID, function: function, line: line)
+      throw error
+    }
+  }
+#endif
--- Sources/Helpers/TaskLocalHelpers.swift
+++ Sources/Helpers/TaskLocalHelpers.swift
@@ -7,22 +7,42 @@
 
 import Foundation
 
-extension TaskLocal where Value == JSONObject {
-  @discardableResult
-  @inlinable package final func withValue<R>(
-    merging valueDuringOperation: Value,
-    operation: () async throws -> R,
-    isolation: isolated (any Actor)? = #isolation,
-    file: String = #fileID,
-    line: UInt = #line
-  ) async rethrows -> R {
-    let currentValue = wrappedValue
-    return try await withValue(
-      currentValue.merging(valueDuringOperation) { _, new in new },
-      operation: operation,
-      isolation: isolation,
-      file: file,
-      line: line
-    )
+#if compiler(>=6.0)
+  extension TaskLocal where Value == JSONObject {
+    @discardableResult
+    @inlinable package final func withValue<R>(
+      merging valueDuringOperation: Value,
+      operation: () async throws -> R,
+      isolation: isolated (any Actor)? = #isolation,
+      file: String = #fileID,
+      line: UInt = #line
+    ) async rethrows -> R {
+      let currentValue = wrappedValue
+      return try await withValue(
+        currentValue.merging(valueDuringOperation) { _, new in new },
+        operation: operation,
+        isolation: isolation,
+        file: file,
+        line: line
+      )
+    }
   }
-}
+#else
+  extension TaskLocal where Value == JSONObject {
+    @discardableResult
+    @inlinable package final func withValue<R>(
+      merging valueDuringOperation: Value,
+      operation: () async throws -> R,
+      file: String = #fileID,
+      line: UInt = #line
+    ) async rethrows -> R {
+      let currentValue = wrappedValue
+      return try await withValue(
+        currentValue.merging(valueDuringOperation) { _, new in new },
+        operation: operation,
+        file: file,
+        line: line
+      )
+    }
+  }
+#endif
