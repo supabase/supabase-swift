0560278be991c7f091a1946cd96ffb2d0bfd45e0
Guilherme Souza <grsouza@pm.me>
2024-10-03 07:10:39 -0300

    move encodeMetadata to helpers

--- Sources/Storage/StorageFileApi.swift
+++ Sources/Storage/StorageFileApi.swift
@@ -48,11 +48,6 @@ enum FileUpload {
       formData.append(url, withName: "")
     }
   }
-
-  private func encodeMetadata(_ metadata: JSONObject) -> Data {
-    let encoder = AnyJSON.encoder
-    return (try? encoder.encode(metadata)) ?? "{}".data(using: .utf8)!
-  }
 }
 
 /// Supabase Storage File API
