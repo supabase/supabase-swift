5f113dd84b270f68570642199d186b1669102bfc
Guilherme Souza <grsouza@pm.me>
2024-12-03 13:44:22 -0300

    fix test

--- Tests/HelpersTests/JWTTests.swift
+++ Tests/HelpersTests/JWTTests.swift
@@ -1,13 +1,13 @@
 import XCTest
 
-@testable import Auth
+@testable import Helpers
 
 final class JWTTests: XCTestCase {
   func testDecodeJWT() throws {
     let token =
       "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNjQ4NjQwMDIxLCJzdWIiOiJmMzNkM2VjOS1hMmVlLTQ3YzQtODBlMS01YmQ5MTlmM2Q4YjgiLCJlbWFpbCI6ImhpQGJpbmFyeXNjcmFwaW5nLmNvIiwicGhvbmUiOiIiLCJhcHBfbWV0YWRhdGEiOnsicHJvdmlkZXIiOiJlbWFpbCIsInByb3ZpZGVycyI6WyJlbWFpbCJdfSwidXNlcl9tZXRhZGF0YSI6e30sInJvbGUiOiJhdXRoZW50aWNhdGVkIn0.CGr5zNE5Yltlbn_3Ms2cjSLs_AW9RKM3lxh7cTQrg0w"
-    let jwt = try decode(jwt: token)
+    let jwt = JWT.decodePayload(token)
     let exp = try XCTUnwrap(jwt?["exp"] as? TimeInterval)
-    XCTAssertEqual(exp, 1648640021)
+    XCTAssertEqual(exp, 1_648_640_021)
   }
 }
