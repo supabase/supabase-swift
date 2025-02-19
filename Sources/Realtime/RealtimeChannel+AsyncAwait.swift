9ff51dd4f9eb3580382e7029dadf8f8a1ac1c07d
Lucas Abijmil <lucas.abijmil@gmail.com>
2025-02-19 23:39:26 +0100

    Create proper files for FilerValue & Filter

--- Sources/Realtime/RealtimeChannel+AsyncAwait.swift
+++ Sources/Realtime/RealtimeChannel+AsyncAwait.swift
@@ -7,45 +7,6 @@
 
 import Foundation
 
-/// A value that can be used to filter Realtime changes in a channel.
-public protocol RealtimeFilterValue {
-  var rawValue: String { get }
-}
-
-extension String: RealtimeFilterValue {
-  public var rawValue: String { self }
-}
-
-extension Int: RealtimeFilterValue {
-  public var rawValue: String { "\(self)" }
-}
-
-extension Double: RealtimeFilterValue {
-  public var rawValue: String { "\(self)" }
-}
-
-extension Bool: RealtimeFilterValue {
-  public var rawValue: String { "\(self)" }
-}
-
-extension UUID: RealtimeFilterValue {
-  public var rawValue: String { uuidString }
-}
-
-extension Date: RealtimeFilterValue {
-  public var rawValue: String {
-    let formatter = ISO8601DateFormatter()
-    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
-    return formatter.string(from: self)
-  }
-}
-
-extension Array: RealtimeFilterValue where Element: RealtimeFilterValue {
-  public var rawValue: String {
-    map(\.rawValue).joined(separator: ",")
-  }
-}
-
 public enum RealtimeFilter {
   case eq(_ column: String, value: any RealtimeFilterValue)
   case neq(_ column: String, value: any RealtimeFilterValue)
---
+++ Sources/Realtime/RealtimeFilterValue.swift
@@ -0,0 +1,46 @@
+//
+//  RealtimeFilterValue.swift
+//  Supabase
+//
+//  Created by Lucas Abijmil on 19/02/2025.
+//
+
+
+/// A value that can be used to filter Realtime changes in a channel.
+public protocol RealtimeFilterValue {
+  var rawValue: String { get }
+}
+
+extension String: RealtimeFilterValue {
+  public var rawValue: String { self }
+}
+
+extension Int: RealtimeFilterValue {
+  public var rawValue: String { "\(self)" }
+}
+
+extension Double: RealtimeFilterValue {
+  public var rawValue: String { "\(self)" }
+}
+
+extension Bool: RealtimeFilterValue {
+  public var rawValue: String { "\(self)" }
+}
+
+extension UUID: RealtimeFilterValue {
+  public var rawValue: String { uuidString }
+}
+
+extension Date: RealtimeFilterValue {
+  public var rawValue: String {
+    let formatter = ISO8601DateFormatter()
+    formatter.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+    return formatter.string(from: self)
+  }
+}
+
+extension Array: RealtimeFilterValue where Element: RealtimeFilterValue {
+  public var rawValue: String {
+    map(\.rawValue).joined(separator: ",")
+  }
+}
