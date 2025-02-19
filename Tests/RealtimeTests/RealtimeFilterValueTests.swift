7f3887bb833ba593f483b41e4e0e46df33b9e788
Lucas Abijmil <lucas.abijmil@gmail.com>
2025-02-19 23:49:40 +0100

    Add tests

---
+++ Tests/RealtimeTests/RealtimeFilterValueTests.swift
@@ -0,0 +1,28 @@
+//
+//  RealtimeFilterValueTests.swift
+//  Supabase
+//
+//  Created by Lucas Abijmil on 19/02/2025.
+//
+
+import XCTest
+@testable import Realtime
+
+final class RealtimeFilterValueTests: XCTestCase {
+  func testUUID() {
+    XCTAssertEqual(
+      UUID(uuidString: "E621E1F8-C36C-495A-93FC-0C247A3E6E5F")!.rawValue,
+      "E621E1F8-C36C-495A-93FC-0C247A3E6E5F")
+  }
+
+  func testDate() {
+    XCTAssertEqual(
+      Date(timeIntervalSince1970: 1_737_465_985).rawValue,
+      "2025-01-21T13:26:25.000Z"
+    )
+  }
+
+  func testArray() {
+    XCTAssertEqual(["a", "b", "c"].rawValue, "a,b,c")
+  }
+}
