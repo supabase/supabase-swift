7d6870ffa980ad3728c272acc03fc3e3c0251fe4
Guilherme Souza <grsouza@pm.me>
2025-05-28 10:32:15 -0300

    fix(realtime): make Push associated to MainActor (#721)
    * fix(realtime): make Push associated to MainActor
    
    * code format
    
    * skip test in Linux

--- Tests/IntegrationTests/RealtimeIntegrationTests.swift
+++ Tests/IntegrationTests/RealtimeIntegrationTests.swift
@@ -24,247 +24,247 @@ struct TestLogger: SupabaseLogger {
 }
 
 #if !os(Android) && !os(Linux)
-@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-final class RealtimeIntegrationTests: XCTestCase {
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  final class RealtimeIntegrationTests: XCTestCase {
 
-  let testClock = TestClock<Duration>()
+    let testClock = TestClock<Duration>()
 
-  let client = SupabaseClient(
-    supabaseURL: URL(string: DotEnv.SUPABASE_URL) ?? URL(string: "http://localhost:54321")!,
-    supabaseKey: DotEnv.SUPABASE_ANON_KEY
-  )
+    let client = SupabaseClient(
+      supabaseURL: URL(string: DotEnv.SUPABASE_URL) ?? URL(string: "http://localhost:54321")!,
+      supabaseKey: DotEnv.SUPABASE_ANON_KEY
+    )
 
-  override func setUp() {
-    super.setUp()
+    override func setUp() {
+      super.setUp()
 
-    _clock = testClock
-  }
-
-  #if !os(Windows) && !os(Linux) && !os(Android)
-  override func invokeTest() {
-    withMainSerialExecutor {
-      super.invokeTest()
-    }
-  }
-  #endif
-
-  func testDisconnectByUser_shouldNotReconnect() async {
-    await client.realtimeV2.connect()
-    let status: RealtimeClientStatus = client.realtimeV2.status
-    XCTAssertEqual(status, .connected)
-
-    client.realtimeV2.disconnect()
-
-    /// Wait for the reconnection delay
-    await testClock.advance(by: .seconds(RealtimeClientOptions.defaultReconnectDelay))
-
-    XCTAssertEqual(client.realtimeV2.status, .disconnected)
-  }
-
-  func testBroadcast() async throws {
-    let channel = client.realtimeV2.channel("integration") {
-      $0.broadcast.receiveOwnBroadcasts = true
+      _clock = testClock
     }
 
-    let receivedMessagesTask = Task {
-      await channel.broadcastStream(event: "test").prefix(3).collect()
-    }
-
-    await Task.yield()
-
-    await channel.subscribe()
-
-    struct Message: Codable {
-      var value: Int
-    }
-
-    try await channel.broadcast(event: "test", message: Message(value: 1))
-    try await channel.broadcast(event: "test", message: Message(value: 2))
-    try await channel.broadcast(event: "test", message: ["value": 3, "another_value": 42])
-
-    let receivedMessages = try await withTimeout(interval: 5) {
-      await receivedMessagesTask.value
-    }
-
-    assertInlineSnapshot(of: receivedMessages, as: .json) {
-      """
-      [
-        {
-          "event" : "test",
-          "payload" : {
-            "value" : 1
-          },
-          "type" : "broadcast"
-        },
-        {
-          "event" : "test",
-          "payload" : {
-            "value" : 2
-          },
-          "type" : "broadcast"
-        },
-        {
-          "event" : "test",
-          "payload" : {
-            "another_value" : 42,
-            "value" : 3
-          },
-          "type" : "broadcast"
+    #if !os(Windows) && !os(Linux) && !os(Android)
+      override func invokeTest() {
+        withMainSerialExecutor {
+          super.invokeTest()
         }
-      ]
-      """
+      }
+    #endif
+
+    func testDisconnectByUser_shouldNotReconnect() async {
+      await client.realtimeV2.connect()
+      let status: RealtimeClientStatus = client.realtimeV2.status
+      XCTAssertEqual(status, .connected)
+
+      client.realtimeV2.disconnect()
+
+      /// Wait for the reconnection delay
+      await testClock.advance(by: .seconds(RealtimeClientOptions.defaultReconnectDelay))
+
+      XCTAssertEqual(client.realtimeV2.status, .disconnected)
     }
 
-    await channel.unsubscribe()
-  }
+    func testBroadcast() async throws {
+      let channel = client.realtimeV2.channel("integration") {
+        $0.broadcast.receiveOwnBroadcasts = true
+      }
 
-  func testBroadcastWithUnsubscribedChannel() async throws {
-    let channel = client.realtimeV2.channel("integration") {
-      $0.broadcast.acknowledgeBroadcasts = true
+      let receivedMessagesTask = Task {
+        await channel.broadcastStream(event: "test").prefix(3).collect()
+      }
+
+      await Task.yield()
+
+      await channel.subscribe()
+
+      struct Message: Codable {
+        var value: Int
+      }
+
+      try await channel.broadcast(event: "test", message: Message(value: 1))
+      try await channel.broadcast(event: "test", message: Message(value: 2))
+      try await channel.broadcast(event: "test", message: ["value": 3, "another_value": 42])
+
+      let receivedMessages = try await withTimeout(interval: 5) {
+        await receivedMessagesTask.value
+      }
+
+      assertInlineSnapshot(of: receivedMessages, as: .json) {
+        """
+        [
+          {
+            "event" : "test",
+            "payload" : {
+              "value" : 1
+            },
+            "type" : "broadcast"
+          },
+          {
+            "event" : "test",
+            "payload" : {
+              "value" : 2
+            },
+            "type" : "broadcast"
+          },
+          {
+            "event" : "test",
+            "payload" : {
+              "another_value" : 42,
+              "value" : 3
+            },
+            "type" : "broadcast"
+          }
+        ]
+        """
+      }
+
+      await channel.unsubscribe()
     }
 
-    struct Message: Codable {
-      var value: Int
+    func testBroadcastWithUnsubscribedChannel() async throws {
+      let channel = client.realtimeV2.channel("integration") {
+        $0.broadcast.acknowledgeBroadcasts = true
+      }
+
+      struct Message: Codable {
+        var value: Int
+      }
+
+      try await channel.broadcast(event: "test", message: Message(value: 1))
+      try await channel.broadcast(event: "test", message: Message(value: 2))
+      try await channel.broadcast(event: "test", message: ["value": 3, "another_value": 42])
     }
 
-    try await channel.broadcast(event: "test", message: Message(value: 1))
-    try await channel.broadcast(event: "test", message: Message(value: 2))
-    try await channel.broadcast(event: "test", message: ["value": 3, "another_value": 42])
-  }
+    func testPresence() async throws {
+      let channel = client.realtimeV2.channel("integration") {
+        $0.broadcast.receiveOwnBroadcasts = true
+      }
 
-  func testPresence() async throws {
-    let channel = client.realtimeV2.channel("integration") {
-      $0.broadcast.receiveOwnBroadcasts = true
-    }
+      let receivedPresenceChangesTask = Task {
+        await channel.presenceChange().prefix(4).collect()
+      }
 
-    let receivedPresenceChangesTask = Task {
-      await channel.presenceChange().prefix(4).collect()
-    }
+      await Task.yield()
 
-    await Task.yield()
+      await channel.subscribe()
 
-    await channel.subscribe()
+      struct UserState: Codable, Equatable {
+        let email: String
+      }
 
-    struct UserState: Codable, Equatable {
-      let email: String
-    }
+      try await channel.track(UserState(email: "test@supabase.com"))
+      try await channel.track(["email": "test2@supabase.com"])
 
-    try await channel.track(UserState(email: "test@supabase.com"))
-    try await channel.track(["email": "test2@supabase.com"])
+      await channel.untrack()
 
-    await channel.untrack()
+      let receivedPresenceChanges = try await withTimeout(interval: 5) {
+        await receivedPresenceChangesTask.value
+      }
 
-    let receivedPresenceChanges = try await withTimeout(interval: 5) {
-      await receivedPresenceChangesTask.value
-    }
-
-    let joins = try receivedPresenceChanges.map { try $0.decodeJoins(as: UserState.self) }
-    let leaves = try receivedPresenceChanges.map { try $0.decodeLeaves(as: UserState.self) }
-    expectNoDifference(
-      joins,
-      [
-        [],  // This is the first PRESENCE_STATE event.
-        [UserState(email: "test@supabase.com")],
-        [UserState(email: "test2@supabase.com")],
-        [],
-      ]
-    )
-
-    expectNoDifference(
-      leaves,
-      [
-        [],  // This is the first PRESENCE_STATE event.
-        [],
-        [UserState(email: "test@supabase.com")],
-        [UserState(email: "test2@supabase.com")],
-      ]
-    )
-
-    await channel.unsubscribe()
-  }
-
-  func testPostgresChanges() async throws {
-    let channel = client.realtimeV2.channel("db-changes")
-
-    let receivedInsertActions = Task {
-      await channel.postgresChange(InsertAction.self, schema: "public").prefix(1).collect()
-    }
-
-    let receivedUpdateActions = Task {
-      await channel.postgresChange(UpdateAction.self, schema: "public").prefix(1).collect()
-    }
-
-    let receivedDeleteActions = Task {
-      await channel.postgresChange(DeleteAction.self, schema: "public").prefix(1).collect()
-    }
-
-    let receivedAnyActionsTask = Task {
-      await channel.postgresChange(AnyAction.self, schema: "public").prefix(3).collect()
-    }
-
-    await Task.yield()
-    await channel.subscribe()
-
-    struct Entry: Codable, Equatable {
-      let key: String
-      let value: AnyJSON
-    }
-
-    // Wait until a system event for makind sure DB change listeners are set before making DB changes.
-    _ = await channel.system().first(where: { _ in true })
-
-    let key = try await
-      (client.from("key_value_storage")
-      .insert(["key": AnyJSON.string(UUID().uuidString), "value": "value1"]).select().single()
-      .execute().value as Entry).key
-    try await client.from("key_value_storage").update(["value": "value2"]).eq("key", value: key)
-      .execute()
-    try await client.from("key_value_storage").delete().eq("key", value: key).execute()
-
-    let insertedEntries = try await receivedInsertActions.value.map {
-      try $0.decodeRecord(
-        as: Entry.self,
-        decoder: JSONDecoder()
+      let joins = try receivedPresenceChanges.map { try $0.decodeJoins(as: UserState.self) }
+      let leaves = try receivedPresenceChanges.map { try $0.decodeLeaves(as: UserState.self) }
+      expectNoDifference(
+        joins,
+        [
+          [],  // This is the first PRESENCE_STATE event.
+          [UserState(email: "test@supabase.com")],
+          [UserState(email: "test2@supabase.com")],
+          [],
+        ]
       )
-    }
-    let updatedEntries = try await receivedUpdateActions.value.map {
-      try $0.decodeRecord(
-        as: Entry.self,
-        decoder: JSONDecoder()
+
+      expectNoDifference(
+        leaves,
+        [
+          [],  // This is the first PRESENCE_STATE event.
+          [],
+          [UserState(email: "test@supabase.com")],
+          [UserState(email: "test2@supabase.com")],
+        ]
       )
-    }
-    let deletedEntryIds = await receivedDeleteActions.value.compactMap {
-      $0.oldRecord["key"]?.stringValue
-    }
 
-    expectNoDifference(insertedEntries, [Entry(key: key, value: "value1")])
-    expectNoDifference(updatedEntries, [Entry(key: key, value: "value2")])
-    expectNoDifference(deletedEntryIds, [key])
-
-    let receivedAnyActions = await receivedAnyActionsTask.value
-    XCTAssertEqual(receivedAnyActions.count, 3)
-
-    if case let .insert(action) = receivedAnyActions[0] {
-      let record = try action.decodeRecord(as: Entry.self, decoder: JSONDecoder())
-      expectNoDifference(record, Entry(key: key, value: "value1"))
-    } else {
-      XCTFail("Expected a `AnyAction.insert` on `receivedAnyActions[0]`")
+      await channel.unsubscribe()
     }
 
-    if case let .update(action) = receivedAnyActions[1] {
-      let record = try action.decodeRecord(as: Entry.self, decoder: JSONDecoder())
-      expectNoDifference(record, Entry(key: key, value: "value2"))
-    } else {
-      XCTFail("Expected a `AnyAction.update` on `receivedAnyActions[1]`")
-    }
+    func testPostgresChanges() async throws {
+      let channel = client.realtimeV2.channel("db-changes")
 
-    if case let .delete(action) = receivedAnyActions[2] {
-      expectNoDifference(key, action.oldRecord["key"]?.stringValue)
-    } else {
-      XCTFail("Expected a `AnyAction.delete` on `receivedAnyActions[2]`")
-    }
+      let receivedInsertActions = Task {
+        await channel.postgresChange(InsertAction.self, schema: "public").prefix(1).collect()
+      }
 
-    await channel.unsubscribe()
+      let receivedUpdateActions = Task {
+        await channel.postgresChange(UpdateAction.self, schema: "public").prefix(1).collect()
+      }
+
+      let receivedDeleteActions = Task {
+        await channel.postgresChange(DeleteAction.self, schema: "public").prefix(1).collect()
+      }
+
+      let receivedAnyActionsTask = Task {
+        await channel.postgresChange(AnyAction.self, schema: "public").prefix(3).collect()
+      }
+
+      await Task.yield()
+      await channel.subscribe()
+
+      struct Entry: Codable, Equatable {
+        let key: String
+        let value: AnyJSON
+      }
+
+      // Wait until a system event for makind sure DB change listeners are set before making DB changes.
+      _ = await channel.system().first(where: { _ in true })
+
+      let key = try await
+        (client.from("key_value_storage")
+        .insert(["key": AnyJSON.string(UUID().uuidString), "value": "value1"]).select().single()
+        .execute().value as Entry).key
+      try await client.from("key_value_storage").update(["value": "value2"]).eq("key", value: key)
+        .execute()
+      try await client.from("key_value_storage").delete().eq("key", value: key).execute()
+
+      let insertedEntries = try await receivedInsertActions.value.map {
+        try $0.decodeRecord(
+          as: Entry.self,
+          decoder: JSONDecoder()
+        )
+      }
+      let updatedEntries = try await receivedUpdateActions.value.map {
+        try $0.decodeRecord(
+          as: Entry.self,
+          decoder: JSONDecoder()
+        )
+      }
+      let deletedEntryIds = await receivedDeleteActions.value.compactMap {
+        $0.oldRecord["key"]?.stringValue
+      }
+
+      expectNoDifference(insertedEntries, [Entry(key: key, value: "value1")])
+      expectNoDifference(updatedEntries, [Entry(key: key, value: "value2")])
+      expectNoDifference(deletedEntryIds, [key])
+
+      let receivedAnyActions = await receivedAnyActionsTask.value
+      XCTAssertEqual(receivedAnyActions.count, 3)
+
+      if case let .insert(action) = receivedAnyActions[0] {
+        let record = try action.decodeRecord(as: Entry.self, decoder: JSONDecoder())
+        expectNoDifference(record, Entry(key: key, value: "value1"))
+      } else {
+        XCTFail("Expected a `AnyAction.insert` on `receivedAnyActions[0]`")
+      }
+
+      if case let .update(action) = receivedAnyActions[1] {
+        let record = try action.decodeRecord(as: Entry.self, decoder: JSONDecoder())
+        expectNoDifference(record, Entry(key: key, value: "value2"))
+      } else {
+        XCTFail("Expected a `AnyAction.update` on `receivedAnyActions[1]`")
+      }
+
+      if case let .delete(action) = receivedAnyActions[2] {
+        expectNoDifference(key, action.oldRecord["key"]?.stringValue)
+      } else {
+        XCTFail("Expected a `AnyAction.delete` on `receivedAnyActions[2]`")
+      }
+
+      await channel.unsubscribe()
+    }
   }
-}
 #endif
--- Tests/RealtimeTests/_PushTests.swift
+++ Tests/RealtimeTests/_PushTests.swift
@@ -12,91 +12,85 @@ import XCTest
 
 @testable import Realtime
 
-@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
-final class _PushTests: XCTestCase {
-  var ws: FakeWebSocket!
-  var socket: RealtimeClientV2!
+#if !os(Android) && !os(Linux) && !os(Windows)
+  @MainActor
+  @available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+  final class _PushTests: XCTestCase {
+    var ws: FakeWebSocket!
+    var socket: RealtimeClientV2!
 
-  #if !os(Windows) && !os(Linux) && !os(Android)
-  override func invokeTest() {
-    withMainSerialExecutor {
-      super.invokeTest()
+    override func setUp() {
+      super.setUp()
+
+      let (client, server) = FakeWebSocket.fakes()
+      ws = server
+
+      socket = RealtimeClientV2(
+        url: URL(string: "https://localhost:54321/v1/realtime")!,
+        options: RealtimeClientOptions(
+          headers: ["apiKey": "apikey"]
+        ),
+        wsTransport: { _, _ in client },
+        http: HTTPClientMock()
+      )
+    }
+
+    func testPushWithoutAck() async {
+      let channel = RealtimeChannelV2(
+        topic: "realtime:users",
+        config: RealtimeChannelConfig(
+          broadcast: .init(acknowledgeBroadcasts: false),
+          presence: .init(),
+          isPrivate: false
+        ),
+        socket: socket,
+        logger: nil
+      )
+      let push = PushV2(
+        channel: channel,
+        message: RealtimeMessageV2(
+          joinRef: nil,
+          ref: "1",
+          topic: "realtime:users",
+          event: "broadcast",
+          payload: [:]
+        )
+      )
+
+      let status = await push.send()
+      XCTAssertEqual(status, .ok)
+    }
+
+    func testPushWithAck() async {
+      let channel = RealtimeChannelV2(
+        topic: "realtime:users",
+        config: RealtimeChannelConfig(
+          broadcast: .init(acknowledgeBroadcasts: true),
+          presence: .init(),
+          isPrivate: false
+        ),
+        socket: socket,
+        logger: nil
+      )
+      let push = PushV2(
+        channel: channel,
+        message: RealtimeMessageV2(
+          joinRef: nil,
+          ref: "1",
+          topic: "realtime:users",
+          event: "broadcast",
+          payload: [:]
+        )
+      )
+
+      let task = Task {
+        await push.send()
+      }
+      await Task.megaYield()
+      push.didReceive(status: .ok)
+
+      let status = await task.value
+      XCTAssertEqual(status, .ok)
     }
   }
-  #endif
-
-  override func setUp() {
-    super.setUp()
-
-    let (client, server) = FakeWebSocket.fakes()
-    ws = server
-
-    socket = RealtimeClientV2(
-      url: URL(string: "https://localhost:54321/v1/realtime")!,
-      options: RealtimeClientOptions(
-        headers: ["apiKey": "apikey"]
-      ),
-      wsTransport: { _, _ in client },
-      http: HTTPClientMock()
-    )
-  }
-
-  func testPushWithoutAck() async {
-    let channel = RealtimeChannelV2(
-      topic: "realtime:users",
-      config: RealtimeChannelConfig(
-        broadcast: .init(acknowledgeBroadcasts: false),
-        presence: .init(),
-        isPrivate: false
-      ),
-      socket: socket,
-      logger: nil
-    )
-    let push = PushV2(
-      channel: channel,
-      message: RealtimeMessageV2(
-        joinRef: nil,
-        ref: "1",
-        topic: "realtime:users",
-        event: "broadcast",
-        payload: [:]
-      )
-    )
-
-    let status = await push.send()
-    XCTAssertEqual(status, .ok)
-  }
-
-  // FIXME: Flaky test, it fails some time due the task scheduling, even tho we're using withMainSerialExecutor.
-  //  func testPushWithAck() async {
-  //    let channel = RealtimeChannelV2(
-  //      topic: "realtime:users",
-  //      config: RealtimeChannelConfig(
-  //        broadcast: .init(acknowledgeBroadcasts: true),
-  //        presence: .init(),
-  //        isPrivate: false
-  //      ),
-  //      socket: Socket(client: socket),
-  //      logger: nil
-  //    )
-  //    let push = PushV2(
-  //      channel: channel,
-  //      message: RealtimeMessageV2(
-  //        joinRef: nil,
-  //        ref: "1",
-  //        topic: "realtime:users",
-  //        event: "broadcast",
-  //        payload: [:]
-  //      )
-  //    )
-  //
-  //    let task = Task {
-  //      await push.send()
-  //    }
-  //    await Task.yield()
-  //    await push.didReceive(status: .ok)
-  //
-  //    let status = await task.value
-  //    XCTAssertEqual(status, .ok)
-  //  }
-}
+#endif
