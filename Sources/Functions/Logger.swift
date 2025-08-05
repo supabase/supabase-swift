7a66e71b78618275427908e0648e9b67adcd2042
Guilherme Souza <guilherme@supabase.io>
2025-08-05 08:30:36 -0300

    move baseURL to Client type

---
+++ Sources/Functions/Logger.swift
@@ -0,0 +1,13 @@
+//
+//  Logger.swift
+//  Supabase
+//
+//  Created by Guilherme Souza on 05/08/25.
+//
+
+import Logging
+
+extension Logger {
+  /// A Logger instance for the Functions module.
+  static let functions = Logger(label: "Functions")
+}
