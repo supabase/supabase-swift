53197e8ff97f91e4646ad3616f3bc9347c4d2a83
Guilherme Souza <guilherme@supabase.io>
2025-08-22 06:20:33 -0300

    refactor: update Functions module for Alamofire integration
    - Update FunctionsClient for Alamofire compatibility
    - Refactor FunctionInvokeOptionsTests and FunctionsClientTests
    - Improve error handling and request formatting in Functions module

--- Tests/FunctionsTests/FunctionInvokeOptionsTests.swift
+++ Tests/FunctionsTests/FunctionInvokeOptionsTests.swift
@@ -1,3 +1,4 @@
+import Alamofire
 import HTTPTypes
 import XCTest
 
@@ -6,13 +7,13 @@ import XCTest
 final class FunctionInvokeOptionsTests: XCTestCase {
   func test_initWithStringBody() {
     let options = FunctionInvokeOptions(body: "string value")
-    XCTAssertEqual(options.headers[.contentType], "text/plain")
+    XCTAssertEqual(options.headers["Content-Type"], "text/plain")
     XCTAssertNotNil(options.body)
   }
 
   func test_initWithDataBody() {
     let options = FunctionInvokeOptions(body: "binary value".data(using: .utf8)!)
-    XCTAssertEqual(options.headers[.contentType], "application/octet-stream")
+    XCTAssertEqual(options.headers["Content-Type"], "application/octet-stream")
     XCTAssertNotNil(options.body)
   }
 
@@ -21,7 +22,7 @@ final class FunctionInvokeOptionsTests: XCTestCase {
       let value: String
     }
     let options = FunctionInvokeOptions(body: Body(value: "value"))
-    XCTAssertEqual(options.headers[.contentType], "application/json")
+    XCTAssertEqual(options.headers["Content-Type"], "application/json")
     XCTAssertNotNil(options.body)
   }
 
@@ -32,12 +33,12 @@ final class FunctionInvokeOptionsTests: XCTestCase {
       headers: ["Content-Type": contentType],
       body: "binary value".data(using: .utf8)!
     )
-    XCTAssertEqual(options.headers[.contentType], contentType)
+    XCTAssertEqual(options.headers["Content-Type"], contentType)
     XCTAssertNotNil(options.body)
   }
 
   func testMethod() {
-    let testCases: [FunctionInvokeOptions.Method: HTTPTypes.HTTPRequest.Method] = [
+    let testCases: [FunctionInvokeOptions.Method: Alamofire.HTTPMethod] = [
       .get: .get,
       .post: .post,
       .put: .put,
