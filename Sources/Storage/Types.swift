ab7fe32bae903cc9a127b227d41b7d3b48e3df23
Guilherme Souza <guilherme@supabase.io>
2026-07-06 08:25:49 -0300

    docs(storage): use inline cspell:ignore for DocC hash suffix in Types.swift

--- Sources/Storage/Types.swift
+++ Sources/Storage/Types.swift
@@ -172,7 +172,7 @@ public struct FileOptions: Sendable {
 
 /// A single signed URL returned as part of a batch sign operation.
 ///
-/// Returned by ``StorageFileApi/createSignedURLs(paths:expiresIn:download:cacheNonce:)-5lkmo``
+/// Returned by ``StorageFileApi/createSignedURLs(paths:expiresIn:download:cacheNonce:)-5lkmo`` // cspell:ignore lkmo
 /// (the legacy `[SignedURL]` overload). Prefer the ``SignedURLResult`` overload for new code.
 ///
 /// ## Topics
