ef6b3337d07f8459ff9ba7f6314f486ed2e91cf2
Guilherme Souza <guilherme@supabase.io>
2026-07-29 19:28:36 -0300

    Revert "fix(storage): use String.date for Bucket.init(fromGenerated:) timestamps"
    This reverts commit 661e038766ff6311a523e4d05b98ecb71948ba6d.

--- Tests/StorageTests/StorageBucketAPITests.swift
+++ Tests/StorageTests/StorageBucketAPITests.swift
@@ -209,8 +209,6 @@ final class StorageBucketAPITests: XCTestCase {
     let buckets = try await storage.listBuckets()
     XCTAssertEqual(buckets.count, 1)
     XCTAssertEqual(buckets[0].name, "test-bucket")
-    XCTAssertEqual(buckets[0].createdAt, "2024-01-01T00:00:00.000Z".date!)
-    XCTAssertEqual(buckets[0].updatedAt, "2024-01-01T00:00:00.000Z".date!)
   }
 
   func testCreateBucket() async throws {
