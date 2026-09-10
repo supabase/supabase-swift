187bfc51062cf558517a207d41a03ea4449f0dbe
Guilherme Souza <guilherme@supabase.io>
2026-09-09 22:23:04 -0300

    feat(functions)!: replace fetch with transport and stream responses through it

--- Tests/FunctionsTests/FunctionsClientLoggerTests.swift
+++ Tests/FunctionsTests/FunctionsClientLoggerTests.swift
@@ -1,5 +1,7 @@
 import Foundation
+import HTTPTypes
 import Logging
+import TestHelpers
 import Testing
 
 @testable import Functions
@@ -56,11 +58,8 @@ struct FunctionsClientLoggerTests {
       url: url,
       headers: ["apikey": apiKey],
       logger: logger,
-      fetch: { request in
-        (
-          Data(),
-          HTTPURLResponse(url: request.url!, statusCode: 200, httpVersion: nil, headerFields: nil)!
-        )
+      transport: ClosureTransport { _, _ in
+        (HTTPTypes.HTTPResponse(status: .ok), nil)
       }
     )
 
